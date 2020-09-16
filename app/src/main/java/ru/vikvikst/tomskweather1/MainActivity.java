359c719d7614e1d238465a2fb3b5d4eb6fd54cf5
Viktor Statkevich <vikvikst474@gmail.com>
2020-09-17 03:12:19 +0700

    method getWindDireciton() in MainActivity is all ready

--- app/src/main/java/ru/vikvikst/tomskweather1/MainActivity.java
+++ app/src/main/java/ru/vikvikst/tomskweather1/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -72,8 +73,20 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < 7; i++) {
                 Log.d(TAG, "item : " + i + " = " + elements1.get(i).text());
             }
-            //температура сегодня ночь день
+            //++температура сегодня ночь день
             Log.d(TAG, "Температура сегодня ночью " + elements1.get(2).children().get(0).text() + ", Температура днем " + elements1.get(2).children().get(2).text());
+            //++ветер
+            Log.d(TAG, "wind " + elements1.get(3).children().text());
+
+            Log.d(TAG, "направление " + elements1.get(3).getAllElements().text());
+            Log.d(TAG, "направление " + elements1.get(3).getAllElements().size());
+            String direction = elements1.get(3).getAllElements().text();
+            String[] direcionWInd = getWindDireciton(direction);
+            Log.d(TAG, "direction 1" + direcionWInd[0]+ "T");
+            Log.d(TAG, "direction 2" + direcionWInd[1]+ "T");
+
+
+
 
 
 
@@ -83,5 +96,14 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+    //returns 2 values in string array . example: южный,юго-западный
+    private String[] getWindDireciton(String direction) {
+        String[] arr = direction.split("м/c");
+        String text = arr[1].trim();
+        text = text.substring(0, text.lastIndexOf(' '));
+        text.replaceAll("\\s","");
+        return text.split(",");
     }
 }
