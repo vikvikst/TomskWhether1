ff6c3f34c814d87001b1c754d881bb0ade4c5cd8
Viktor Statkevich <vikvikst474@gmail.com>
2020-09-16 21:29:27 +0700

    add view of weather today in activity_main.xml

--- app/src/main/java/ru/vikvikst/tomskweather1/MainActivity.java
+++ app/src/main/java/ru/vikvikst/tomskweather1/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.w3c.dom.Document;
+
 public class MainActivity extends AppCompatActivity {
+
+    public String TAG = "mylog";
+
+    private Document document;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
